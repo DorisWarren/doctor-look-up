d72b9e5099de688d9bcf4b7c8e576c00362ae37d
Doris Warren <doriswarren11@gmail.com>
2019-04-02 20:33:44 -0700

    new api call using doc last name and symptom - trying to fix promise and condence to one input

--- src/main.js
+++ src/main.js
@@ -5,21 +5,21 @@ import './styles.css';
 import {UserSearch} from "./scripts.js";
 
 $(document).ready(function() {
-  $('.results').hide();
+  // $('.results').hide();
   let newSearch = new UserSearch();
   $('#feeling-dead-button').click(function(){
     event.preventDefault();
     let symptom = $('#symptom-input-dead').val();
-    let promise = newSearch.searchSymptoms();
+    let promise = newSearch.searchSymptoms(symptom);
 
     promise.then(function(response) {
       $('.doc-info').text("");
       let body = JSON.parse(response);
-      let noDocs = true;
-      for(let i = 0; i < 10; i ++){
-        for (let j = 0; j < body.data[i].specialties.length; j++) {
-          if (body.data[i].specialties[j].description.match(symptom)) {
-            console.log(body.data[i]);
+      // let noDocs = true;
+      // // for(let i = 0; i < 10; i ++){
+      //   for (let j = 0; j < body.data[i].specialties.length; j++) {
+      //     if (body.data[i].specialties[j].description.match(symptom)) {
+      //       console.log(body.data[i]);
             $('.doc-info').append('<img src="' + body.data[i].profile.image_url + '">');
             $('.doc-info').append('<p>' + body.data[i].practices[0].name + " " + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + "," + body.data[i].practices[0].visit_address.state + " " + "Phone Number:" + " " + body.data[i].practices[0].phones[0].number + " " + "New patient availability:" + body.data[i].practices[0].accept_new_patients + '</p>');
             // noDocs = false;
@@ -36,32 +36,32 @@ $(document).ready(function() {
     });
     $('.results').show();
   });
-  $('#hot-doc-button').click(function(){
-    event.preventDefault();
-    let doctorLastName = $('#docLastNameInput').val();
-    let promise = newSearch.searchSymptoms();
-
-    promise.then(function(response) {
-      $('.doc-info').text("");
-      let body = JSON.parse(response);
-      let noDocs = true;
-      for(let i = 0; i < 10; i ++){
-        for (let j = 0; j < body.data[i].specialties.length; j++) {
-          console.log(body.data[i].practices[0]);
-          if (body.data[i].practices[0].name.includes(doctorLastName)) {
-            $('.doc-info').append('<img src="' + body.data[i].profile.image_url + '">');
-            $('.doc-info').append('<p>' + body.data[i].practices[0].name + " " + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + "," + body.data[i].practices[0].visit_address.state + " " + "Phone Number:" + " " + body.data[i].practices[0].phones[0].number + " " + "New patient availability:" + body.data[i].practices[0].accept_new_patients + '</p>');
-            noDocs = false;
-          }
-        }
-      }
-      if (noDocs) {
-        $('.doc-info').text("Sorry, There are no doctors that match your request.");
-      }
-    },
-    function(error){
-      $('.showErrors').text('There was an error processing your request: ${error.message}');
-    });
-    $('.results').show();
-  });
+  // $('#hot-doc-button').click(function(){
+  //   event.preventDefault();
+  //   let doctorLastName = $('#docLastNameInput').val();
+  //   let promise = newSearch.searchSymptoms();
+  //
+  //   promise.then(function(response) {
+  //     $('.doc-info').text("");
+  //     let body = JSON.parse(response);
+  //     let noDocs = true;
+  //     for(let i = 0; i < 10; i ++){
+  //       for (let j = 0; j < body.data[i].specialties.length; j++) {
+  //         console.log(body.data[i].practices[0]);
+  //         if (body.data[i].practices[0].name.includes(doctorLastName)) {
+  //           $('.doc-info').append('<img src="' + body.data[i].profile.image_url + '">');
+  //           $('.doc-info').append('<p>' + body.data[i].practices[0].name + " " + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + "," + body.data[i].practices[0].visit_address.state + " " + "Phone Number:" + " " + body.data[i].practices[0].phones[0].number + " " + "New patient availability:" + body.data[i].practices[0].accept_new_patients + '</p>');
+  //           noDocs = false;
+  //         }
+  //       }
+  //     }
+  //     if (noDocs) {
+  //       $('.doc-info').text("Sorry, There are no doctors that match your request.");
+  //     }
+  //   },
+  //   function(error){
+  //     $('.showErrors').text('There was an error processing your request: ${error.message}');
+  //   });
+  //   $('.results').show();
+  // });
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,36 +1,36 @@
 export class UserSearch{
 
-  constructor() {
-  }
-  searchSymptoms(){
+  // constructor() {
+  // }
+  searchSymptoms(docLastName,symptom-dead){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.520247,-122.674195,100&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${docLastName}&query=${symptom-dead}&location=or-portland&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
         } else {
           reject(Error(request.statusText));
         }
-      };
+      }
       request.open("GET", url, true);
       request.send();
     });
   }
 }
- //  searchDoctors(){
- //     return new Promise(function(resolve, reject) {
- //       let request = new XMLHttpRequest();
- //       let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.520247,-122.674195,100&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
- //       request.onload = function() {
- //         if (this.status === 200) {
- //           resolve(request.response);
- //         } else {
- //           reject(Error(request.statusText));
- //         }
- //       };
- //       request.open("GET", url, true);
- //       request.send();
- //     });
- //   }
- // }
+  // searchDoctors(){
+  //    return new Promise(function(resolve, reject) {
+  //      let request = new XMLHttpRequest();
+  //      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.520247,-122.674195,100&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+  //      request.onload = function() {
+  //        if (this.status === 200) {
+  //          resolve(request.response);
+  //        } else {
+  //          reject(Error(request.statusText));
+  //        }
+  //      };
+  //      request.open("GET", url, true);
+  //      request.send();
+  //    });
+  //  }
+ }
