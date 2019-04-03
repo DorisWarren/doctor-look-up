ad5790ef472a40da355a3786fdb109c72f25ea19
Doris Warren <doriswarren11@gmail.com>
2019-04-03 08:59:14 -0700

    add first name to api key

--- src/main.js
+++ src/main.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
   $('#symptom-form').submit(function(event){
     event.preventDefault();
     let symptom = $('#symptomDead').val();
-    // let docFirstName = $('#docFirstName').val();
+    let docFirstName = $('#docFirstName').val();
     let docLastName = $('#docLastName').val();
 
     $('#symptomDead').val("");
-    // $('#docFirstName').val("");
+    $('#docFirstName').val("");
     $('#docLastName').val("");
 
     let newSearch = new UserSearch();
-    let promise = newSearch.searchSymptoms(symptom,docLastName);
+    let promise = newSearch.searchSymptoms(symptom,docFirstName,docLastName);
 
     promise.then(function(response) {
       $('#doc-info').text("");
@@ -34,34 +34,5 @@ $(document).ready(function() {
     function(error){
       $('.showErrors').text('There was an error processing your request: ${error.message}');
     });
-    // $('.results').show();
-  });
-  // $('#hot-doc-button').click(function(){
-  //   event.preventDefault();
-  //   let doctorLastName = $('#docLastNameInput').val();
-  //   let promise = newSearch.searchSymptoms();
-  //
-  //   promise.then(function(response) {
-  //     $('.doc-info').text("");
-  //     let body = JSON.parse(response);
-  //     let noDocs = true;
-  //     for(let i = 0; i < 10; i ++){
-  //       for (let j = 0; j < body.data[i].specialties.length; j++) {
-  //         console.log(body.data[i].practices[0]);
-  //         if (body.data[i].practices[0].name.includes(doctorLastName)) {
-  //           $('.doc-info').append('<img src="' + body.data[i].profile.image_url + '">');
-  //           $('.doc-info').append('<p>' + body.data[i].practices[0].name + " " + body.data[i].practices[0].visit_address.street + " " + body.data[i].practices[0].visit_address.city + "," + body.data[i].practices[0].visit_address.state + " " + "Phone Number:" + " " + body.data[i].practices[0].phones[0].number + " " + "New patient availability:" + body.data[i].practices[0].accept_new_patients + '</p>');
-  //           noDocs = false;
-  //         }
-  //       }
-  //     }
-  //     if (noDocs) {
-  //       $('.doc-info').text("Sorry, There are no doctors that match your request.");
-  //     }
-  //   },
-  //   function(error){
-  //     $('.showErrors').text('There was an error processing your request: ${error.message}');
-  //   });
-  //   $('.results').show();
-  // });
+  });  
 });
--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,10 @@
 export class UserSearch{
 
-  searchSymptoms(symptomDead, docLastName){
+  searchSymptoms(symptomDead, docFirstName, docLastName){
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       let apiKey = process.env.exports.apiKey;
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${docLastName}&query=${symptomDead}&location=or-portland&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?last_name=${docLastName}&first_name=${docFirstName}&query=${symptomDead}&location=or-portland&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -17,21 +17,3 @@ export class UserSearch{
     });
   }
 }
-//
-// 37.773,-122.413,100
-// 37.773,-122.413
-  // searchDoctors(){
-  //    return new Promise(function(resolve, reject) {
-  //      let request = new XMLHttpRequest();
-  //      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=45.520247,-122.674195,100&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-  //      request.onload = function() {
-  //        if (this.status === 200) {
-  //          resolve(request.response);
-  //        } else {
-  //          reject(Error(request.statusText));
-  //        }
-  //      };
-  //      request.open("GET", url, true);
-  //      request.send();
-  //    });
-  //  }
